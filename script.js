63ac529aecf469a3319afb2cd7f9bc0f44bcfac9
netali1053 <natalihot1991@mail.ru>
2024-11-25 13:28:59 +0300

    Отправка анкеты POST-запросом

--- script.js
+++ script.js
@@ -1,8 +1,52 @@
 const form = document.querySelector(".form");
-form.addEventListener("submit", (event) => {
-  // Предотвращает действие браузера по умолчанию. В данном случае — отправку формы
-  // https://learn.javascript.ru/default-browser-action
-  event.preventDefault();
+const nameInput = document.querySelector('#name');
+const email = document.querySelector('#email');
+const secondName = document.querySelector('#secondName');
+const phone = document.querySelector('#phone');
+const agreeCheckbox = document.querySelector('#agree');
+const sendBtn = document.querySelector('#button');
+const clearBtn = document.querySelector('#clear');
+const info = document.querySelector('.info-container');
+const title = document.querySelector('h1');
 
-  // Здесь твой код
-});
+form.addEventListener("submit", async function(event) {
+    event.preventDefault();
+
+    sendBtn.addEventListener('click', async function() {
+        try {
+            const request = await fetch(`https://polinashneider.space/user`, {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer: netali1053'
+                },
+                body: JSON.stringify({
+                    "name": nameInput.value,
+                    "secondName": secondName.value,
+                    "phone": phone.value,
+                    "email": email.value,
+                    "agree": agreeCheckbox.checked
+                }),
+            })
+
+            const data = await request.json();
+
+            if (data) {
+                info.classList.remove('hidden');
+                title.classList.add('hidden');
+                form.classList.add('hidden');
+                form.reset();
+
+                const textOk = document.createElement('p');
+                textOk.textContent = '✨Поздравляем! Ваша анкета успешно отправлена!✨';
+                textOk.classList.add('paragraph');
+
+                info.append(textOk);
+            }
+
+        } catch (error) {
+            alert("Произошла ошибка!", error)
+        }
+    })
+})
